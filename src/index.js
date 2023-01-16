a6843c170c4013ff3d83762e90a25900783b27e9
merv-e <34655454+merv-e@users.noreply.github.com>
2023-01-16 13:18:50 +0100

    name of a function updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(reducer, middleware);
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
 
